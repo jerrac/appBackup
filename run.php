f7bbc186c2c3f2c31dbdf85c31033463ad654069
David Reagan <david@reagannetworks.com>
2016-07-08 15:16:07 -0700

    Moved exec call and associated logging to a function.

--- run.php
+++ run.php
@@ -3,8 +3,6 @@
  * appBackup
  * Description: A simple php script to archive mysql based applications.
  * @author David Reagan <david@reagannetworks.com>
- * Date: 7/8/16
- * Time: 10:42 AM
  */
 if (isset($argv[1])) {
     for ($i = 1; $i < $argc; $i++) {
@@ -34,28 +32,31 @@ function logAppBackupMessage($logFile, $appName, $level, $logMessage)
     fwrite($logFile, json_encode($message) . "\n");
 }
 
+function execVarCustomCommand($logFile,$name,$command,$opTag){
+    $outputVar = array();
+    $returnVar = false;
+    $execVar = exec($command, $outputVar, $returnVar);
+    if (!empty($outputVar)) {
+        logAppBackupMessage($logFile, $name, 'info',
+          array('outputVar' => $outputVar, 'opTag' => $opTag));
+    }
+    if (!empty($returnVar)) {
+        logAppBackupMessage($logFile, $name, 'info',
+          array('returnVar' => $returnVar, 'opTag' => $opTag));
+    }
+    if (!empty($execVar)) {
+        logAppBackupMessage($logFile, $name, 'info',
+          array('execVar' => $execVar, 'opTag' => $opTag));
+    } elseif ($execVar === false) {
+        logAppBackupMessage($logFile, $name, 'error',
+          array('message' => 'Exec command returned FALSE', 'opTag' => $opTag));
+    }
+}
+
 function copyFiles($logFile, $name, $source, $destination)
 {
-    $copyFilesOutput = array();
-    $copyFilesReturn = false;
     $copyFilesCommand = "cp -r $source $destination";
-    $copyFilesExec = exec($copyFilesCommand, $copyFilesOutput,
-      $copyFilesReturn);
-    if (!empty($copyFilesOutput)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('copyFilesOutput' => $copyFilesOutput));
-    }
-    if (!empty($copyFilesReturn)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('copyFilesReturn' => $copyFilesReturn));
-    }
-    if (!empty($copyFilesExec)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('mysqlExec' => $copyFilesExec));
-    } elseif ($copyFilesExec === false) {
-        logAppBackupMessage($logFile, $name, 'error',
-          "copyFilesExec command returned FALSE for source $source");
-    }
+    execVarCustomCommand($logFile,$name,$copyFilesCommand,"copy files");
 }
 
 include 'config.php';
@@ -78,27 +79,9 @@ foreach ($apps['apps'] as $name => $val) {
         break;
     }
     if ($val['backupDatabase']) {
-        $mysqldumpOutput = array();
-        $mysqldumpReturn = false;
         $mysqldumpPath = $tempDir . '/' . $date . '-' . $name . '.sql';
         $mysqldumpCommand = 'mysqldump -u ' . $val['database']['dbuser'] . ' -h' . $val['database']['dbhost'] . ' -p' . $val['database']['dbpass'] . ' ' . $val['database']['dbname'] . '> ' . $mysqldumpPath;
-        $mysqlExec = exec($mysqldumpCommand, $mysqldumpOutput,
-          $mysqldumpReturn);
-        if (!empty($mysqldumpOutput)) {
-            logAppBackupMessage($logFile, $name, 'info',
-              array('mysqldumpOutput' => $mysqldumpOutput));
-        }
-        if (!empty($mysqldumpReturn)) {
-            logAppBackupMessage($logFile, $name, 'info',
-              array('mysqldumpReturn' => $mysqldumpReturn));
-        }
-        if (!empty($mysqlExec)) {
-            logAppBackupMessage($logFile, $name, 'info',
-              array('mysqlExec' => $mysqlExec));
-        } elseif ($mysqlExec === false) {
-            logAppBackupMessage($logFile, $name, 'error',
-              "mysqlDumpExec command returned FALSE.");
-        }
+        execVarCustomCommand($logFile,$name,$mysqldumpCommand,"mysqldump");
     }
     if ($val['backupData']) {
         //Check if data is inside code. If it is, skip. Otherwise, copy.
@@ -115,44 +98,9 @@ foreach ($apps['apps'] as $name => $val) {
         copyFiles($logFile, $name, $val['code'], $dataDestination);
     }
     // Create tar.gz file.
-    $tarOutput = array();
-    $tarReturn = false;
     $tarCommand = 'tar -C tmp --remove-files -cpzf ' . $val['tmp'] . '/' . $date . '-' . $name . '.tar.gz ' . $date . '-' . $name;
-    $tarExec = exec($tarCommand, $tarOutput, $tarReturn);
-    if (!empty($tarOutput)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('tarOutput' => $tarOutput));
-    }
-    if (!empty($tarReturn)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('tarReturn' => $tarReturn));
-    }
-    if (!empty($tarExec)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('tarExec' => $tarExec));
-    } elseif ($tarExec === false) {
-        logAppBackupMessage($logFile, $name, 'error',
-          "tarExec command returned FALSE");
-    }
-
+    execVarCustomCommand($logFile,$name,$tarCommand,"create tar archive");
     // Move to store
-    $moveOutput = array();
-    $moveReturn = false;
     $moveCommand = 'mv ' . $val['tmp'] . '/' . $date . '-' . $name . '.tar.gz ' . $val['store'];
-    $moveExec = exec($moveCommand, $moveOutput, $moveReturn);
-    if (!empty($moveOutput)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('moveOutput' => $moveOutput));
-    }
-    if (!empty($moveReturn)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('moveReturn' => $moveReturn));
-    }
-    if (!empty($moveExec)) {
-        logAppBackupMessage($logFile, $name, 'info',
-          array('moveExec' => $moveExec));
-    } elseif ($moveExec === false) {
-        logAppBackupMessage($logFile, $name, 'error',
-          "moveExec command returned FALSE");
-    }
+    execVarCustomCommand($logFile,$name,$moveCommand,"move tar archive to store");
 }
